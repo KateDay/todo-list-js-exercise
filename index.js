7ecce776ff29bbd35e7fc65cacfef33b86b8793f
Kate <khummle@gmail.com>
2019-05-28 16:10:46 +0000

    playing with outputs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function newTask(title, description) {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
-    markCompleted: function(){
+    markCompleted: function() {
       this.complete = true;
     }
   };
@@ -22,9 +22,16 @@ function newTask(title, description) {
 const task1 = newTask("Clean Cat Litter", "take all the 💩 out of the liltter box");
 const task2 = newTask("Do Laundry", "😨")
 const task3 = newTask("Clean the kitchen", "just put it all in the dishwasher!")
-const tasks = [task1, task2, task3];
+const tasks = [task1.title, task1.description, task2.title, task2.description, task3.title, task3.description];
 
-task1.logState();
+
 task1.markCompleted();
 task1.logState();
 
+task2.markCompleted();
+task2.logState();
+
+task3.markCompleted();
+task3.logState();
+
+console.log(tasks);
